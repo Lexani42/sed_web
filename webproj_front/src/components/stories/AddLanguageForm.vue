47cf6d84bf47faa69a937c2d3f950c2520b9f397
Lexani42 <33593776+Lexani42@users.noreply.github.com>
2025-02-11 16:44:31 +0200

    Change list of languages in Add Language Form

--- webproj_front/src/components/stories/AddLanguageForm.vue
+++ webproj_front/src/components/stories/AddLanguageForm.vue
@@ -63,15 +63,8 @@ const { currentStory: story } = storeToRefs(storyStore)
 
 const languages = [
   { code: 'en', name: 'English' },
-  { code: 'es', name: 'Spanish' },
-  { code: 'fr', name: 'French' },
-  { code: 'de', name: 'German' },
-  { code: 'it', name: 'Italian' },
-  { code: 'pt', name: 'Portuguese' },
   { code: 'ru', name: 'Russian' },
-  { code: 'zh', name: 'Chinese' },
-  { code: 'ja', name: 'Japanese' },
-  { code: 'ko', name: 'Korean' }
+  { code: 'ua', name: 'Ukrainian' },
 ]
 
 const formats = ['text', 'audio']
